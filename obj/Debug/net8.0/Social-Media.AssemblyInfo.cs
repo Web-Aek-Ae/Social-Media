912cdcf76b3f45e7ea05cdd59126af893a816963
Perawat Kosit <65010768@kmitl.ac.th>
2024-02-22 22:05:08 +0700

    test

--- obj/Debug/net8.0/Social-Media.AssemblyInfo.cs
+++ obj/Debug/net8.0/Social-Media.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Social-Media")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24d89ebe8c22435b0637977502e49f03d1834696")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b335ec6f5020990615ccb30cd79f2fd9d41e1e7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Social-Media")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Social-Media")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

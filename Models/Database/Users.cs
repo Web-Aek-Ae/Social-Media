0569b86ebf20cf5166276f6631d3246937a00703
mewpk <milzaza123@gmail.com>
2024-02-29 02:28:53 +0700

    Fix required attribute in User and ViewModels

--- Models/Database/Users.cs
+++ Models/Database/Users.cs
@@ -1,4 +1,4 @@
-using System;
+
 using System.ComponentModel.DataAnnotations;
 
 namespace SocialMedia.Models.Database
@@ -8,19 +8,19 @@ namespace SocialMedia.Models.Database
         public int UserId { get; set; }
 
         [Required(ErrorMessage = "Name is required")]
-        public string Name {get; set;}
+        public required string Name {get; set;}
 
         [Required(ErrorMessage = "Username is required")]
-        public string Username { get; set; }
+        public required string Username { get; set; }
 
         [Required(ErrorMessage = "Email is required")]
          [EmailAddress(ErrorMessage = "Invalid email address")]
-        public string Email { get; set; }
+        public required string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
         [DataType(DataType.Password)]
         [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
-        public string Password {get; set;}
+        public required string Password {get; set;}
 
         }
 
--- Models/ViewModels.cs
+++ Models/ViewModels.cs
@@ -12,33 +12,33 @@ namespace SocialMedia.ViewModels
     public int UserId { get; set; }
 
     [Required(ErrorMessage = "Name is required")]
-    public string Name { get; set; }
+    public required string Name { get; set; }
 
     [Required(ErrorMessage = "Username is required")]
-    public string Username { get; set; }
+    public required string Username { get; set; }
 
     [Required(ErrorMessage = "Email is required")]
     [EmailAddress(ErrorMessage = "Invalid email address")]
-    public string Email { get; set; }
+    public required string Email { get; set; }
 
     [Required(ErrorMessage = "Password is required")]
     [DataType(DataType.Password)]
     [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
-    public string Password { get; set; }
+    public required string Password { get; set; }
 
     [Required(ErrorMessage = "Please confirm password")]
     [DataType(DataType.Password)]
     [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-    public string ConfirmPassword { get; set; }
+    public required string ConfirmPassword { get; set; }
   }
   public class LoginViewModel
   {
     [Required(ErrorMessage = "Username is required")]
-    public string Username { get; set; }
+    public required string Username { get; set; }
 
     [Required]
     [DataType(DataType.Password)]
-    public string Password { get; set; }
+    public required string Password { get; set; }
   }
 
 }

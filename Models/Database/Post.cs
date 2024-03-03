8648b9ea44dd8f76d72bac6b485a9172f5a3e042
mewpk <milzaza123@gmail.com>
2024-03-03 21:32:15 +0700

    Fix required attribute in models

--- Models/Database/Post.cs
+++ Models/Database/Post.cs
@@ -18,16 +18,16 @@ namespace SocialMedia.Models.Database
 
         [Required(ErrorMessage = "Title is required")]
         [StringLength(255, ErrorMessage = "Title cannot be longer than 255 characters.")] // Limit title length
-        public string Title { get; set; }
+        public required string Title { get; set; }
 
         [Required(ErrorMessage = "Content is required")]
         [DataType(DataType.MultilineText)] // Specifies that the data type is a multiline text, useful for UI hints
-        public string Content { get; set; }
+        public required string Content { get; set; }
 
         [StringLength(500, ErrorMessage = "Location cannot be longer than 500 characters.")] // Optionally limit location length
-        public string Location { get; set; }
+        public string? Location { get; set; }
 
-        public string Image { get; set; } // Consider validating the image URL or path if applicable
+        public string? Image { get; set; } // Consider validating the image URL or path if applicable
 
         [Range(1, int.MaxValue, ErrorMessage = "MaxPeople must be at least 1")] // Ensures MaxPeople is a positive number
         public int MaxPeople { get; set; }
--- Models/Database/Users.cs
+++ Models/Database/Users.cs
@@ -12,23 +12,23 @@ namespace SocialMedia.Models.Database
 
         [Required(ErrorMessage = "Name is required")]
         [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters")]
-        public string Name { get; set; }
+        public required string Name { get; set; }
 
         [Required(ErrorMessage = "Username is required")]
         [StringLength(50, MinimumLength = 4, ErrorMessage = "Username must be between 4 and 50 characters")]
         [RegularExpression("^[a-zA-Z0-9]*$", ErrorMessage = "Username must be alphanumeric")]
-        public string Username { get; set; }
+        public required string Username { get; set; }
 
         [Required(ErrorMessage = "Email is required")]
         [EmailAddress(ErrorMessage = "Invalid email address")]
         [StringLength(255, ErrorMessage = "Email cannot exceed 255 characters")]
-        public string Email { get; set; }
+        public required string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
         [DataType(DataType.Password)]
         [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
         // Do not add MaxLength for password because it will be hashed
-        public string Password { get; set; }
+        public required string Password { get; set; }
 
     }
 }

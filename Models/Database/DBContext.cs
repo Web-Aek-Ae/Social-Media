89fe0a31427edb361151753fff5048b7def87a6d
mewpk <milzaza123@gmail.com>
2024-03-03 21:38:56 +0700

    Add Group and GroupMember models to DBContext

--- Models/Database/DBContext.cs
+++ Models/Database/DBContext.cs
@@ -12,6 +12,7 @@ namespace SocialMedia.Models.Database
         public DbSet<User> Users { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<PostLike> PostLikes { get; set; }
+        public DbSet<GroupMember> GroupMembers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -36,6 +37,26 @@ namespace SocialMedia.Models.Database
                 .HasOne<User>(pl => pl.User)
                 .WithMany(u => u.PostLikes)
                 .HasForeignKey(pl => pl.UserId);
+
+            // Configuring the many-to-many relationship between Users and Groups through GroupMembers
+            modelBuilder.Entity<GroupMember>()
+                .HasKey(gm => new { gm.UserId, gm.GroupId }); // Composite key
+
+            modelBuilder.Entity<GroupMember>()
+                .HasOne<User>(gm => gm.User)
+                .WithMany(u => u.GroupMembers)
+                .HasForeignKey(gm => gm.UserId);
+
+            modelBuilder.Entity<GroupMember>()
+                .HasOne<Group>(gm => gm.Group)
+                .WithMany(g => g.Members)
+                .HasForeignKey(gm => gm.GroupId);
+
+            // Optionally, configure the Group entity if needed
+            // For example, setting up a unique constraint, indexes, or configuring property behaviors
+            // modelBuilder.Entity<Group>()
+            //     .HasIndex(g => g.Name)
+            //     .IsUnique(); // Only if you want group names to be unique
 
             // Add any additional model configuration here
         }
--- Models/Database/Users.cs
+++ Models/Database/Users.cs
@@ -10,6 +10,8 @@ namespace SocialMedia.Models.Database
         public virtual ICollection<Post> Posts { get; set; }
         public virtual ICollection<PostLike> PostLikes { get; set; }
 
+        public virtual ICollection<GroupMember> GroupMembers { get; set; }
+
         [Required(ErrorMessage = "Name is required")]
         [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters")]
         public required string Name { get; set; }

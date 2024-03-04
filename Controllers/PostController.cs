f79f17acfd717fd9d40e3326aaa910db8a9e77d9
mewpk <milzaza123@gmail.com>
2024-03-05 00:45:39 +0700

    Add username to ViewData in Create action

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using SocialMedia.Services;
 using SocialMedia.ViewModels; // Namespace where TableViewModel is located
 using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace SocialMedia.Controllers
 {
@@ -15,6 +16,11 @@ namespace SocialMedia.Controllers
 
         public IActionResult Create()
         {
+            var username = HttpContext.User.Identity?.Name;
+            // Alternatively, if the username is stored in a specific claim type
+            var specificClaimUsername = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            ViewData["Username"] = username;
             return View();
         }
     }

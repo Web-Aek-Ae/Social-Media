ada0538d274532b5f61d8145ff185c7b5b3c975d
mewpk <milzaza123@gmail.com>
2024-03-05 01:05:20 +0700

    Refactor PostController and PostViewModel

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,14 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using SocialMedia.Services;
+using SocialMedia.Models.Database; // Namespace where Post is located
 using SocialMedia.ViewModels; // Namespace where TableViewModel is located
 using System.Threading.Tasks;
 using System.Security.Claims;
+
 
 namespace SocialMedia.Controllers
 {
     public class PostController : Controller
     {
+        private readonly PostService _postService;
 
+        public PostController(PostService postService)
+        {
+            _postService = postService;
+        }
         public IActionResult Index()
         {
             return View();
@@ -22,6 +29,32 @@ namespace SocialMedia.Controllers
 
             ViewData["Username"] = username;
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreatePost([FromBody] PostViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var post = new Post
+                {
+                    UserId = model.UserId,
+                    Title = model.Title,
+                    Content = model.Content, // Corrected from Description to Content
+                    Location = model.Location,
+                    Image = model.Image,
+                    MaxPeople = model.MaxPeople, // Assuming MaxPeople is non-nullable
+                    Date = model.Date, // Assuming Date is non-nullable
+                    ExpireDate = model.ExpireDate // Assuming ExpireDate is non-nullable
+                };
+
+                await _postService.MakePost(post);
+
+                return Json(new { success = true, message = "Post created successfully!" });
+            }
+
+            return Json(new { success = false, message = "Model state is not valid." });
         }
     }
 

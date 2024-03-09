4d76b2f2f83e717e6347d0c1cce0508bfac824bb
wwanellope <panapa1409@gmail.com>
2024-03-10 04:27:32 +0700

    Debug pop-up in home/page

--- wwwroot/lib/ApplicationClosed.js
+++ wwwroot/lib/ApplicationClosed.js
@@ -1,16 +1,14 @@
-var btnEl = document.getElementsByClassName("applications-closed");
+var btnApp = document.getElementsByClassName("applications-closed");
 var btnCancel = document.getElementById("btnCC");
 
-let state = 0; 
 function open() {
     document.getElementById("applicationsClosed").style.visibility = "visible";
     document.getElementsByClassName("modalBg")[0].style.visibility = "visible"
 }
-
 function close(){
     document.getElementById("applicationsClosed").style.visibility = "hidden"
     document.getElementsByClassName("modalBg")[0].style.visibility = "hidden"
 }
 
-btnEl[0].addEventListener('click', open);
+btnApp[0].addEventListener('click', open);
 btnCancel.addEventListener('click', close);

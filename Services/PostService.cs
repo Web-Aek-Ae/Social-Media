4cf2832e6c8261bb103ff18961b1654434276b85
perawat_kosit <perawat246@gmail.com>
2024-03-11 02:41:38 +0700

    update

--- Services/PostService.cs
+++ Services/PostService.cs
@@ -30,7 +30,14 @@ namespace SocialMedia.Services
         
         public List<Post> GetPostsByGroupId(int id)
         {
-            return _context.Posts.Include(p => p.Group).Include(p => p.User).Include(p => p.PostLikes).Include(p => p.JoinActivities).Where(p => p.GroupId == id).ToList();
+            return _context.Posts
+            .Include(p => p.Group)
+            .Include(p => p.User)
+            .Include(p => p.PostLikes).ThenInclude(pl => pl.User)
+            .Include(p => p.JoinActivities)
+            .Include(p => p.Comments).ThenInclude(c => c.User)
+            .Include(p => p.Category)
+            .Where(p => p.GroupId == id).ToList();
         }
         public async Task<bool> MakePost(Post post)
         {

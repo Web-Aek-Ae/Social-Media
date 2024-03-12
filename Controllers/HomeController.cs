9dc6365cbe1fc9cbe7d289a61b8344a4882be1ac
Watchanon Jingjit <65010973@kmitl.ac.th>
2024-03-12 22:05:51 +0700

    pull model

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using SocialMedia.Models;
 using System.Security.Claims;
 using SocialMedia.Services;
+using SocialMedia.Models.Database;
 namespace SocialMedia.Controllers;
 
 [Authorize]
@@ -11,21 +12,26 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
     private readonly PostService _postService;
-    public HomeController(ILogger<HomeController> logger , PostService postService)
+    private readonly UserService _userService;
+    public HomeController(ILogger<HomeController> logger, PostService postService, UserService userService)
     {
         _logger = logger;
         _postService = postService;
+        _userService = userService;
     }
+
 
     public IActionResult Index()
     {
-        var username = HttpContext.User.Identity?.Name;
-        _logger.LogInformation($"Username from JWT: {username}");
+        var UserId = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
 
-        
-        ViewData["Username"] = username;
-        var posts = _postService.GetAllPosts();
-        return View(posts); // Passes posts as a model to the view
+        if (UserId == null)
+        {
+            return RedirectToAction("Login", "User");
+        }
+
+        var user = _userService.GetUserById(int.Parse(UserId));
+        return View(user);
     }
     public IActionResult Privacy()
     {
@@ -38,13 +44,14 @@ public class HomeController : Controller
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 
-    public IActionResult Post(){
-    var username = HttpContext.User.Identity?.Name;
-    // Alternatively, if the username is stored in a specific claim type
-    var UserId = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
-    // Use the username for your application logic...
-    ViewData["UserId"] = UserId;
-    ViewData["Username"] = username;
-    return View();
+    public IActionResult Post()
+    {
+        var username = HttpContext.User.Identity?.Name;
+        // Alternatively, if the username is stored in a specific claim type
+        var UserId = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+        // Use the username for your application logic...
+        ViewData["UserId"] = UserId;
+        ViewData["Username"] = username;
+        return View();
     }
 }

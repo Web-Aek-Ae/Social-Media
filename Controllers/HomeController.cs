c782aa54cd213fa868e13abb18aa1fe44b98ace0
ppookkiesbubu <pooky21147@gmail.com>
2024-03-12 22:34:59 +0700

    ja merge main

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,8 +2,10 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SocialMedia.Models;
+using SocialMedia.ViewModels;
 using System.Security.Claims;
 using SocialMedia.Services;
+using SocialMedia.Models.Database;
 namespace SocialMedia.Controllers;
 
 [Authorize]
@@ -11,7 +13,8 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
     private readonly PostService _postService;
-    public HomeController(ILogger<HomeController> logger , PostService postService)
+    private readonly UserService _userService;
+    public HomeController(ILogger<HomeController> logger, PostService postService)
     {
         _logger = logger;
         _postService = postService;
@@ -22,10 +25,33 @@ public class HomeController : Controller
         var username = HttpContext.User.Identity?.Name;
         _logger.LogInformation($"Username from JWT: {username}");
 
-        
+        var UserId = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+
         ViewData["Username"] = username;
+        ViewData["UserId"] = UserId;
+
+        if (UserId == null)
+        {
+            return RedirectToAction("Login", "User");
+        }
+
+        var user = _userService.GetUserById(int.Parse(UserId));
+
+        if (user == null)
+        {
+            return RedirectToAction("Login", "User");
+
+        }
+        var activity = new List<JoinActivity>();
+
         var posts = _postService.GetAllPosts();
-        return View(posts); // Passes posts as a model to the view
+        var model = new HomeViewModel
+        {
+            Posts = posts,
+            Activities = activity
+        };
+
+        return View(model); // Passes posts as a model to the view
     }
     public IActionResult Privacy()
     {
@@ -38,13 +64,14 @@ public class HomeController : Controller
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 
-    public IActionResult Post(){
-    var username = HttpContext.User.Identity?.Name;
-    // Alternatively, if the username is stored in a specific claim type
-    var UserId = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
-    // Use the username for your application logic...
-    ViewData["UserId"] = UserId;
-    ViewData["Username"] = username;
-    return View();
+    public IActionResult Post()
+    {
+        var username = HttpContext.User.Identity?.Name;
+        // Alternatively, if the username is stored in a specific claim type
+        var UserId = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+        // Use the username for your application logic...
+        ViewData["UserId"] = UserId;
+        ViewData["Username"] = username;
+        return View();
     }
 }

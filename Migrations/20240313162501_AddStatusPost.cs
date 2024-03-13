f868cac17c1353e81f43f4ebc70b06563d8d0572
mewpk <milzaza123@gmail.com>
2024-03-13 23:25:15 +0700

    Add PostStatus property to Post model

---
+++ Migrations/20240313162501_AddStatusPost.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Social_Media.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PostStatus",
+                table: "Posts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PostStatus",
+                table: "Posts");
+        }
+    }
+}

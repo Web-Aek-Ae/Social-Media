17bb5f24502624e7aec1d6bf4ffcd82fb43092c1
24thofmayy <patthananchualam@gmail.com>
2024-03-13 23:31:22 +0700

    add three dot in group's details page(unsuccessful)

---
+++ wwwroot/lib/deleteGroup.js
@@ -0,0 +1,13 @@
+let toggle = 0;
+function openmembers(){
+    if (toggle == 1){
+        document.getElementsByClassName("delete-group-pop-up")[0].style.visibility = "hidden";
+        document.getElementsByClassName("popup-backdrop")[0].style.visibility = "hidden";
+        toggle = 0;
+    }
+    else{
+        document.querySelector(".delete-group-pop-up").style.visibility = "visible";
+        document.querySelector(".popup-backdrop").style.visibility = "visible"; 
+        toggle = 1;
+    }
+}

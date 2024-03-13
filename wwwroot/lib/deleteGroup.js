9de528c576314d6f67211b5c727cd3a4970588d8
24thofmayy <patthananchualam@gmail.com>
2024-03-14 00:46:44 +0700

    add pop up in delete group

--- wwwroot/lib/deleteGroup.js
+++ wwwroot/lib/deleteGroup.js
@@ -1,13 +1,14 @@
-let toggle = 0;
-function openmembers(){
-    if (toggle == 1){
-        document.getElementsByClassName("delete-group-pop-up")[0].style.visibility = "hidden";
-        document.getElementsByClassName("popup-backdrop")[0].style.visibility = "hidden";
-        toggle = 0;
-    }
-    else{
-        document.querySelector(".delete-group-pop-up").style.visibility = "visible";
-        document.querySelector(".popup-backdrop").style.visibility = "visible"; 
-        toggle = 1;
-    }
+var btnDelete = document.getElementsByClassName("delete-group");
+var btnCancel = document.getElementById("btnCancle");
+
+function open() {
+    document.getElementById("delete").style.visibility = "visible";
+    document.getElementsByClassName("popup-backdrop")[0].style.visibility = "visible"
 }
+function close(){
+    document.getElementById("delete").style.visibility = "hidden"
+    document.getElementsByClassName("popup-backdrop")[0].style.visibility = "hidden"
+}
+
+btnDelete[0].addEventListener('click', open);
+btnCancel.addEventListener('click', close);
